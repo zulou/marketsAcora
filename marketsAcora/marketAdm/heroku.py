8f33777cea9226d0a5ed58e669434b2bee6eadb5
zulou <rmil.cb@gmail.com>
2018-07-05 17:03:08 -0500

    postgress

--- marketsAcora/marketAdm/heroku.py
+++ marketsAcora/marketAdm/heroku.py
@@ -9,3 +9,9 @@ DEBUG = False
 MIDDLEWARE += [ # noqa: W0401
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES = {
+    'default': db_from_env
+}
